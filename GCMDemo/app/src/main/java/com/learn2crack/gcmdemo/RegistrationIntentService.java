773908c5817910f13411d1a5b80f9e10b63605a9
rakave <velu.ravichandran@chicagobooth.edu>
2018-01-28 13:41:08 +0530

    Working

--- GCMDemo/app/src/main/java/com/learn2crack/gcmdemo/RegistrationIntentService.java
+++ GCMDemo/app/src/main/java/com/learn2crack/gcmdemo/RegistrationIntentService.java
@@ -50,7 +50,7 @@ public class RegistrationIntentService extends IntentService{
         requestBody.setRegistrationId(registrationId);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("http://192.168.0.111:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
